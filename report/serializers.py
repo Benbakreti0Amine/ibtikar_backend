b22e529277a1524882d20f55de4bf011a91765a1
Bencharef fatima <96701637+Th3-l4dy@users.noreply.github.com>
2024-12-16 07:00:50 +0100

    Update serializers.py
    cleaning

--- report/serializers.py
+++ report/serializers.py
@@ -1,14 +1,4 @@
 
-# from rest_framework import serializers
-# from .models import Report
-# from users.models import User
-
-# class ReportSerializer(serializers.ModelSerializer):
-#     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
-
-#     class Meta:
-#         model = Report
-#         fields = ['id', 'title', 'description', 'severity', 'latitude', 'longitude', 'image', 'video', 'user']
 
 
 from rest_framework import serializers
