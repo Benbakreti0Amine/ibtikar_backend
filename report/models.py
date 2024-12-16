2c83cc836b41c1764ba6629cc17373899d55fb01
Bencharef fatima <96701637+Th3-l4dy@users.noreply.github.com>
2024-12-16 07:00:28 +0100

    Update models.py
    cleaning

--- report/models.py
+++ report/models.py
@@ -1,39 +1,5 @@
 
 
-# from django.db import models
-# from users.models import User  
-# class Report(models.Model):
-#     SEVERITY_CHOICES = [
-#         ('Low', 'Low'),
-#         ('Medium', 'Medium'),
-#         ('High', 'High'),
-#         ('Critical', 'Critical'),
-#     ]
-
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
-#     severity = models.CharField(max_length=10, choices=SEVERITY_CHOICES)
-#     latitude = models.DecimalField(max_digits=9, decimal_places=6)  
-#     longitude = models.DecimalField(max_digits=9, decimal_places=6)  
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     image = models.ImageField(upload_to='reports/images/', blank=True, null=True)
-#     video = models.FileField(upload_to='reports/videos/', blank=True, null=True)
-    
-    
-#     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  
-
-#     def save(self, *args, **kwargs):
-#         super().save(*args, **kwargs)
-#         self.generate_alert()
-
-#     def generate_alert(self):
-#         from alert.models import Alert  
-#         Alert.objects.create(
-#             report=self,
-#             message=f"An incident has been reported near this location: {self.title}"
-#         )
-
 
 
 from django.db import models
