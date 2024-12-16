dba6b998394a57c86ef0050ac8d003652fec904b
Bencharef fatima <96701637+Th3-l4dy@users.noreply.github.com>
2024-12-16 06:57:27 +0100

    Update urls.py
    cleaning

--- alert/urls.py
+++ alert/urls.py
@@ -1,16 +1,4 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import AlertViewSet
 
-# router = DefaultRouter()
-# router.register(r'', AlertViewSet, basename='alert')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-
-# users/urls.py
 from django.urls import path
 from .views import UpdateLocationView
 
