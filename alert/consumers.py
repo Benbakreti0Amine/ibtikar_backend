9c4b604352ed847563bde1b7992cc86932a0bd40
Bencharef fatima <96701637+Th3-l4dy@users.noreply.github.com>
2024-12-16 06:56:58 +0100

    Update consumers.py
    cleaning

--- alert/consumers.py
+++ alert/consumers.py
@@ -1,4 +1,4 @@
-# alerts/consumers.py
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
@@ -6,7 +6,7 @@ class AlertConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope["user"]
         if self.user.is_authenticated:
-            # Join a group specific to the user
+           
             await self.channel_layer.group_add(f"user_{self.user.id}", self.channel_name)
             await self.accept()
         else:
@@ -14,15 +14,12 @@ class AlertConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         if self.user.is_authenticated:
-            # Remove the user from the group
+            
             await self.channel_layer.group_discard(f"user_{self.user.id}", self.channel_name)
 
-    # async def send_alert(self, event):
-    #     # Send alert message to WebSocket
-    #     message = event["message"]
-    #     await self.send(text_data=json.dumps({"message": message}))
+    
     async def send_alert(self, event):
-            # Add the print statement for debugging
+            
             print(f"Alert sent: {event['message']}")
-            # Send the alert message to the WebSocket
+           
             await self.send(text_data=json.dumps({"message": event["message"]}))
