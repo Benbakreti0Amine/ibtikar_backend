ff092bb5f193f8c52604dfc19ef831009994f588
Bencharef fatima <96701637+Th3-l4dy@users.noreply.github.com>
2024-12-16 07:02:29 +0100

    Update settings.py
    cleaning

--- sos/settings.py
+++ sos/settings.py
@@ -3,6 +3,6 @@ EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'f.bencharef@esi-sba.dz'
-EMAIL_HOST_PASSWORD = 'amgp jrjh cmza dchq'  # Use an app password for security
+EMAIL_HOST_PASSWORD = ''  # Use an app password for security # it was removed for security measures 
 EMERGENCY_CONTACT_EMAIL = 'f.bencharef@esi-sba.dz'
 DEFAULT_FROM_EMAIL = 'f.bencharef@esi-sba.dz'
